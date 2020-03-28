272b4a3598820b526b6fa4181754e7812a699cf2
Christian-Cell <cristohp74@gmail.com>
2020-03-28 19:45:15 +0100

    sanp de cadenas

---
+++ archivo.js
@@ -0,0 +1,11 @@
+var a = "Amigo";
+alert(a.toLowerCase());
+console.log(a);
+
+var a = "Amigo";
+alert(a.toUpperCase());
+console.log(a);
+
+var a = "Amigo";
+alert(a.replace("go","ga"));
+console.log(a);
